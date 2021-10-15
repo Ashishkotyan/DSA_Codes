d463aaada6ab80e68acb23813084c4d48d92bce5
Ashish <72222933+AshishDevOp@users.noreply.github.com>
2021-10-15 20:07:33 +0530

    Add files via upload

---
+++ 38 Minimum swaps and K together.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int arr[] = {2, 1, 5, 6, 3}, K = 3;
+ int n=5,Swap=0;
+ 
+ for(int i=0;i<n;i++)
+ {
+ 	if(arr[i]>K)
+ 	{
+ 		
+	 }
+	
+ 	
+ }
+ 
+ while(i<n)
+{
+	if(arr[i]<k)
+	{
+		i++;
+	}
+	else{
+		int temp = 
+	}
+ } 
+
+ 	cout<<Swap;
+}
+
