73fd6343c9c795d32d169e2d0fcd7004db19003a
Ashish <72222933+AshishDevOp@users.noreply.github.com>
2021-10-19 10:27:35 +0530

    Add files via upload

--- 24 find all pairs on integer array whose sum is equal to given number.cpp
+++ 24 find all pairs on integer array whose sum is equal to given number.cpp
@@ -4,20 +4,23 @@ using namespace std;
 int main()
 {
 	
-int	N = 4, K = 2;
- int arr[] = {1, 5, 7, 1};
+int	N = 4, K = 8;
+ int arr[] = {1, 5, 7, 1,};
 // int arr[] = {1, 1, 1, 1};
  int count=0;
+ 
  for(int i=0;i<N;i++)
  {
- 	for(int j=i;j<N;j++)
+ 	for(int j=i+1;j<N;j++)
  	{
  		if(arr[i]+arr[j]==K)
  		{
+ 			cout<<arr[i]<<arr[j]<<" ";
  			count++;
-		 }
-	 }
+		}
+	}
  }
+ 
  	cout<<"The pair is ";
 	cout<<count;
 
