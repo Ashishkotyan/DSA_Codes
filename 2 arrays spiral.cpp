6f11a8515a0cd10a25eced4d77c49ca09bc8ced2
Ashish <72222933+Ashishkotyan@users.noreply.github.com>
2021-12-26 10:21:38 +0530

    Add files via upload

---
+++ 2 arrays spiral.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	int a[n][m];
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		
+			cin>>a[i][j];
+	
+		}
+	
+//	spiral order print
+	int row_st=0, row_end=n-1,cols=0,cole=n-1;
+		
+		while(row_st<=row_end && cols<=cole)
+		{
+			
+			
+		}
+	
+	
+	
+	
+}
