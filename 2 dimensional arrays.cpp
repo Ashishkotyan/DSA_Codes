8e53ef0af6f1588c93ede1d08d71d3954a562ac6
Ashish <72222933+Ashishkotyan@users.noreply.github.com>
2021-12-26 10:21:54 +0530

    Add files via upload

---
+++ 2 dimensional arrays.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	int arr[n][m];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			cin>>arr[i][j];
+		}
+	}
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+		
+		
+		
+	}	
+	
+	int key;
+	cin>>key;
+	bool flag =false;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			if(arr[i][j]==key)
+			{
+				 cout<<i<<" "<<j<<endl;
+				 flag=true;
+				 
+			}
+		}
+		
+	}
+	
+	if(flag){
+		cout<<"Element is Found\n";
+	}
+	else {
+		cout<<"Element is Not Found";
+	}
+	
+	
+}
