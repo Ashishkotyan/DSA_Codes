2d4ce8eb81fa5ca7368d455b23c27f8390e17ac7
Ashish <72222933+Ashishkotyan@users.noreply.github.com>
2022-01-07 09:58:20 +0530

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() //Arrays 
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	int maxi;
+	int mini;
+	for(int i=0;i<n;i++){
+	
+	cin>>arr[i];
+}
+
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]>=arr[0]){
+			maxi =arr[i];
+		}
+	cout<<maxi;
+		
+		
+	}
+
+}
+
